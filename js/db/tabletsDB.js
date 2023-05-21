61c18ea633b1536fccdc052ea218facec3fe60aa
r0tif3r0 <gasertyf@gmail.com>
2023-05-21 15:46:10 +0300

    v.1

--- js/db/tabletsDB.js
+++ js/db/tabletsDB.js
@@ -2,61 +2,61 @@ export const tabletsDB = [
   {
     title: 'Xiaomi Redmi Pad',
     date: '08.04.2023',
-    poster: '../img/tablets-img/xiaomi-redmi-pad.png'
+    poster: 'img/tablets-img/xiaomi-redmi-pad.png'
   },
   {
     title: 'Xiaomi Pad 5',
     date: '11.11.2022',
-    poster: '../img/tablets-img/xiaomi-pad-5.png'
+    poster: 'img/tablets-img/xiaomi-pad-5.png'
   },
   {
     title: 'Samsung Galaxy Tab S6 Lite',
     date: '10.01.2022',
-    poster: '../img/tablets-img/samsung-galaxy-tab-s6-lite.png'
+    poster: 'img/tablets-img/samsung-galaxy-tab-s6-lite.png'
   },
   {
     title: 'Samsung Galaxy Tab S7 FE',
     date: '12.07.2022',
-    poster: '../img/tablets-img/samsung-galaxy-tab-s7-fe.png'
+    poster: 'img/tablets-img/samsung-galaxy-tab-s7-fe.png'
   },
   {
     title: 'Samsung Galaxy Tab A8',
     date: '07.02.2022',
-    poster: '../img/tablets-img/samsung-galaxy-tab-a8.png'
+    poster: 'img/tablets-img/samsung-galaxy-tab-a8.png'
   },
   {
     title: 'Samsung Galaxy Tab S8+',
     date: '19.07.2022',
-    poster: '../img/tablets-img/samsung-galaxy-tab-s8-plus.png'
+    poster: 'img/tablets-img/samsung-galaxy-tab-s8-plus.png'
   },
   {
     title: 'Apple iPad 10.2',
     date: '20.02.2023',
-    poster: '../img/tablets-img/apple-ipad-10.2.png'
+    poster: 'img/tablets-img/apple-ipad-10.2.png'
   },
   {
     title: 'Apple iPad Air',
     date: '17.11.2022',
-    poster: '../img/tablets-img/apple-ipad-air.png'
+    poster: 'img/tablets-img/apple-ipad-air.png'
   },
   {
     title: 'Apple iPad Pro 11',
     date: '12.04.2022',
-    poster: '../img/tablets-img/apple-ipad-pro-11.png'
+    poster: 'img/tablets-img/apple-ipad-pro-11.png'
   },
   {
     title: 'Apple iPad mini',
     date: '23.03.2023',
-    poster: '../img/tablets-img/apple-ipad-mini.png'
+    poster: 'img/tablets-img/apple-ipad-mini.png'
   },
   {
     title: 'HUAWEI MatePad 10.4',
     date: '16.03.2023',
-    poster: '../img/tablets-img/huawei-matepad-10.4.png'
+    poster: 'img/tablets-img/huawei-matepad-10.4.png'
   },
   {
     title: 'Honor Pad X8',
     date: '16.07.2022',
-    poster: '../img/tablets-img/honor-pad-x8.png'
+    poster: 'img/tablets-img/honor-pad-x8.png'
   },
 ]
